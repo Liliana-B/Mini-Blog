03a79a64e0d823a02bd8c4c89064dfe7cf50f2c5
Liliana Brissos <liliana@lilianab.com>
2017-01-18 17:05:21 +1100

    Minified files

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,12 +60,36 @@ files: [
 ],
 tasks: ['sass']
 }
-}
+},
+  uglify: { 
+    my_target : { 
+      options : { 
+        sourceMap : true, 
+        sourceMapName : 'js/sourceMap.map'
+      }, 
+      files : { 
+        'js/core.min.js' : ['js/jquery-1.12.4.min.js', 'js/bootstrap.min.js', 'js/jquery.easing.min.js', 'js/jquery.countdown.min.js', 'js/device.min.js', 'js/form.min.js', 'js/jquery.placeholder.min.js', 'js/jquery.shuffle.min.js', 'js/jquery.parallax.min.js', 'js/jquery.circle-progress.min.js', 'js/jquery.swipebox.min.js', 'js/smoothscroll.min.js', 'js/wow.min.js', 'js/jquery.smartmenus.js', 'js/universal.js'] 
+      } 
+    } 
+  },
+  cssmin: {
+  target: {
+    files: [{
+      expand: true,
+      cwd: 'css/',
+      src: ['*.css', '!*.min.css'],
+      dest: 'css/',
+      ext: '.min.css'
+    }]
+  }
+}  
 });
 // Load externally defined tasks.
 grunt.loadNpmTasks('grunt-sass');
 grunt.loadNpmTasks('grunt-contrib-watch');
 grunt.loadNpmTasks('grunt-contrib-copy');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
 // Establish tasks we can run from the terminal.
 grunt.registerTask('build', ['sass']);
 grunt.registerTask('default', ['build', 'watch']);
